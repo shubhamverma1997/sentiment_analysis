042c4a824485b339e218e0b12fa6516d3c0e5de8
uprasad96 <uprasad96@gmail.com>
2017-03-17 20:51:28 +0530

    Update RnWalk.py

--- RnWalk.py
+++ RnWalk.py
@@ -16,14 +16,17 @@ if __name__ == "__main__":
 		adjList.append(seed[0])		
 	
 	i=1
-	while i<=4:		
-		for a in adjList:
-			temp = []
+	while i<=1:
+		temp = []	
+		for a in adjList:			
 			for syn in wn.synsets(a):				#synonyms
 				word=syn.lemma_names()[0]
 				if word not in graph.nodes():
 					graph.add_node(word)
-					graph.add_edge(a,word,weight=1)
+					wt=syn.path_similarity(Word(a).synsets[0])
+					if wt==None:
+						wt=0
+					graph.add_edge(a,word,weight=round(wt,2))
 					temp.append(word)
 
 			syn=Word(a).synsets[0]
@@ -31,27 +34,38 @@ if __name__ == "__main__":
 			l=syn.lemmas()[0]
 			if l.antonyms():
 				for anyt in l.antonyms():
-					graph.add_node(anyt.name())		#antonyms
-					graph.add_edge(a,anyt.name(),weight=-1)
-					temp.append(anyt.name())
+					if anyt.name() not in graph.nodes():						
+						graph.add_node(anyt.name())		#antonyms
+						wt=syn.path_similarity(Word(anyt.name()).synsets[0])
+						if wt==None:
+							wt=0
+						graph.add_edge(a,anyt.name(),weight=round(wt,2))
+						temp.append(anyt.name())
 
 			for hypr in syn.hypernyms():
-				graph.add_node(hypr.lemma_names()[0])	#hypernyms
-				graph.add_edge(a,hypr.lemma_names()[0],weight=1)
-				temp.append(hypr.lemma_names()[0])
+				if hypr.lemma_names()[0] not in graph.nodes():
+					graph.add_node(hypr.lemma_names()[0])	#hypernyms
+					wt=syn.path_similarity(Word(hypr.lemma_names()[0]).synsets[0])
+					if wt==None:
+						wt=0
+					graph.add_edge(a,hypr.lemma_names()[0],weight=round(wt,2))
+					temp.append(hypr.lemma_names()[0])
 
 			for hypo in syn.hyponyms():
-				graph.add_node(hypo.lemma_names()[0]) 	#hyponyms
-				graph.add_edge(a,hypo.lemma_names()[0],weight=1)
-				temp.append(hypo.lemma_names()[0])
+				if hypo.lemma_names()[0] not in graph.nodes():					
+					graph.add_node(hypo.lemma_names()[0]) 	#hyponyms
+					wt=syn.path_similarity(Word(hypo.lemma_names()[0]).synsets[0])
+					if wt==None:
+						wt=0
+					graph.add_edge(a,hypo.lemma_names()[0],weight=round(wt,2))
+					temp.append(hypo.lemma_names()[0])
 
 			adjList=temp
 
 		i=i+1
-	
-	nx.draw(graph,pos=nx.random_layout(graph), with_labels=True, node_color='#ADD8E6', font_size=6, width=0.2)
+	nx.draw(graph,pos=nx.random_layout(graph),with_labels=True, node_color='#ADD8E6', font_size=5, width=0.2, alpha=0.4)
 
-	plt.savefig("mlpro.png")
+	plt.savefig("RnWalk.png", dpi=1000)
 
 
 
