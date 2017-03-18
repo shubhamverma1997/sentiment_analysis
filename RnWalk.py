933b41e9291213e43cc04cb7db2f07abb7018829
uprasad96 <uprasad96@gmail.com>
2017-03-18 14:25:27 +0530

    Update RnWalk.py
    Dude Recursion ruk nhi rhi

--- RnWalk.py
+++ RnWalk.py
@@ -3,34 +3,37 @@ from nltk.corpus import wordnet as wn
 import networkx as nx
 import matplotlib.pyplot as plt
 from textblob import Word
-from numpy.random import choice
+from random import choice
+import numpy as np
 
 
-def random_W(w,graph):
-
-	nbrs=graph.neighbors(w)
-	transit = []
+def transit_P (i,j,graph):
+	nbrs=graph.neighbors(i)
 	wtsum=0
 	for nb in nbrs:
-		wt=Word(w).synsets[0].path_similarity(Word(nb).synsets[0])
+		wt=(Word(i).synsets[0]).path_similarity(Word(nb).synsets[0])
 		if wt==None:
-			wt=0.1
+			wt=0
 		wtsum=wtsum+wt
+	return ((Word(i).synsets[0]).path_similarity(Word(j).synsets[0]))/wtsum
 
-	for nb in nbrs:
-		wt=Word(w).synsets[0].path_similarity(Word(nb).synsets[0])
-		if wt==None:
-			wt=0.1
-		transit.append(wt/wtsum)
-
-	if len(nbrs):			
-		draw = choice(nbrs, 1, p=transit)
-		for seed in seedList:
-			if draw==seed[0]:
-				return seed[1]
-		random_W(draw,graph)
-	else:
+def hit2plus(i,splus,graph):
+	if i in splus:
 		return 0
+	else:		
+		sum=0
+		for j in graph.nodes():
+			sum=sum+transit_P(i,j,graph)*(hit2plus(j,splus,graph)+1)
+		return sum	
+
+def hit2minus(i,sminus,graph):
+	if i in sminus:
+		return 0
+	else:		
+		sum=0
+		for j in graph.nodes():
+			sum=sum+transit_P(i,j,graph)*(hit2minus(j,sminus,graph)+1)
+		return sum
 
 if __name__ == "__main__":
 
@@ -83,6 +86,14 @@ if __name__ == "__main__":
 	plt.savefig("RnWalk.png", dpi=1000)
 
 	known = seedList
+	splus = []
+	sminus = []
+	for kn in known:
+		if kn[1]==1:
+			splus.append(kn[0])
+		if kn[1]==-1:
+			sminus.append(kn[0])
+
 	unknown = []
 	for w in graph.nodes():
 		if w not in known:
@@ -91,24 +102,23 @@ if __name__ == "__main__":
 	for w in unknown:
 		avg_pos=0
 		avg_neg=0
-		m=1
-		while m<=5:
-			score=random_W(w,graph)
-			if score>0:
-				avg_pos=avg_pos+score
-			else:
-				avg_neg=avg_neg+score
-			m=m+1
+		k=1
+		while k<=5:
+			htime=hit2plus(w,splus,graph)
+			avg_pos=avg_pos+htime
+			k=k+1
+		avg_pos=avg_pos/5
+
+		k=1
+		while k<=5:
+			htime=hit2minus(w,sminus,graph)
+			avg_neg=avg_neg+htime
+			k=k+1
+		avg_neg=avg_neg/5
+
 		if avg_pos>avg_neg:
 			seedList.append([w,1])
 		else:
 			seedList.append([w,-1])
 
 	print(seedList)
-
-
-
-
-
-
-
