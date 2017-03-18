41060fcde9ce6d8e747fe2eb6b3725d5cdebdb5b
uprasad96 <uprasad96@gmail.com>
2017-03-18 18:25:09 +0530

    Update RnWalk.py

--- RnWalk.py
+++ RnWalk.py
@@ -5,35 +5,40 @@ import matplotlib.pyplot as plt
 from textblob import Word
 from random import choice
 import numpy as np
-
+from random import random
+from bisect import bisect
 
 def transit_P (i,j,graph):
 	nbrs=graph.neighbors(i)
 	wtsum=0
 	for nb in nbrs:
-		wt=(Word(i).synsets[0]).path_similarity(Word(nb).synsets[0])
-		if wt==None:
-			wt=0
+		wt=graph[i][nb]['weight']
 		wtsum=wtsum+wt
-	return ((Word(i).synsets[0]).path_similarity(Word(j).synsets[0]))/wtsum
+	return graph[i][j]['weight']/wtsum
 
-def hit2plus(i,splus,graph):
+def hit2plus(i,splus,graph,count):
 	if i in splus:
+		return count
+	elif len(graph.neighbors(i))==0:
 		return 0
-	else:		
-		sum=0
-		for j in graph.nodes():
-			sum=sum+transit_P(i,j,graph)*(hit2plus(j,splus,graph)+1)
-		return sum	
+	else:
+		probs = []
+		for j in graph.neighbors(i):
+			probs.append(transit_P(i,j,graph))
+		draw = np.random.choice(graph.neighbors(i), p=probs)
+		return hit2plus(draw,splus,graph,count+1)	
 
-def hit2minus(i,sminus,graph):
+def hit2minus(i,sminus,graph,count):
 	if i in sminus:
+		return count
+	elif len(graph.neighbors(i))==0:
 		return 0
-	else:		
-		sum=0
-		for j in graph.nodes():
-			sum=sum+transit_P(i,j,graph)*(hit2minus(j,sminus,graph)+1)
-		return sum
+	else:
+		probs = []
+		for j in graph.neighbors(i):
+			probs.append(transit_P(i,j,graph))
+		draw = np.random.choice(graph.neighbors(i), p=probs)
+		return hit2minus(draw,sminus,graph,count+1)	
 
 if __name__ == "__main__":
 
@@ -53,7 +58,7 @@ if __name__ == "__main__":
 				word=syn.lemma_names()[0]
 				if word not in graph.nodes():
 					graph.add_node(word)
-					graph.add_edge(a,word)
+					graph.add_edge(a,word,weight=1)
 					temp.append(word)
 
 			syn=Word(a).synsets[0]
@@ -63,36 +68,43 @@ if __name__ == "__main__":
 				for anyt in l.antonyms():
 					if anyt.name() not in graph.nodes():						
 						graph.add_node(anyt.name())		#antonyms
-						graph.add_edge(a,anyt.name())
+						graph.add_edge(a,anyt.name(),weight=-1)
 						temp.append(anyt.name())
 
 			for hypr in syn.hypernyms():
 				if hypr.lemma_names()[0] not in graph.nodes():
 					graph.add_node(hypr.lemma_names()[0])	#hypernyms
-					graph.add_edge(a,hypr.lemma_names()[0])
+					graph.add_edge(a,hypr.lemma_names()[0],weight=1)
 					temp.append(hypr.lemma_names()[0])
 
 			for hypo in syn.hyponyms():
 				if hypo.lemma_names()[0] not in graph.nodes():					
 					graph.add_node(hypo.lemma_names()[0]) 	#hyponyms
-					graph.add_edge(a,hypo.lemma_names()[0])
+					graph.add_edge(a,hypo.lemma_names()[0],weight=1)
 					temp.append(hypo.lemma_names()[0])
 
 			adjList=temp
 
 		i=i+1
+
+	for m in graph.nodes():
+		for n in graph.nodes():
+			if graph.has_edge(m,n)==False:
+				graph.add_edge(m,n,weight=0)
+
 	nx.draw(graph,pos=nx.random_layout(graph),with_labels=True, node_color='#ADD8E6', font_size=5, width=0.2, alpha=0.4)
 
 	plt.savefig("RnWalk.png", dpi=1000)
 
-	known = seedList
+	known = []
 	splus = []
 	sminus = []
-	for kn in known:
-		if kn[1]==1:
-			splus.append(kn[0])
-		if kn[1]==-1:
-			sminus.append(kn[0])
+	for seed in seedList:
+		known.append(seed[0])
+		if seed[1]==1:
+			splus.append(seed[0])
+		if seed[1]==-1:
+			sminus.append(seed[0])
 
 	unknown = []
 	for w in graph.nodes():
@@ -104,16 +116,16 @@ if __name__ == "__main__":
 		avg_neg=0
 		k=1
 		while k<=5:
-			htime=hit2plus(w,splus,graph)
+			htime=hit2plus(w,splus,graph,0)
 			avg_pos=avg_pos+htime
 			k=k+1
 		avg_pos=avg_pos/5
 
 		k=1
 		while k<=5:
-			htime=hit2minus(w,sminus,graph)
+			htime=hit2minus(w,sminus,graph,0)
 			avg_neg=avg_neg+htime
-			k=k+1
+
 		avg_neg=avg_neg/5
 
 		if avg_pos>avg_neg:
@@ -121,4 +133,4 @@ if __name__ == "__main__":
 		else:
 			seedList.append([w,-1])
 
-	print(seedList)
+	# print(seedList)
